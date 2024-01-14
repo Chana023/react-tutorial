01dfc0d059e275b525a5bc5812ca70ed98108ee6
Chana023 <jesalchana23@gmail.com>
2024-01-14 16:50:10 +0200

    Started Building with Reusable components

---
+++ pdas/src/App.js
@@ -0,0 +1,15 @@
+import ProfileCard from "./ProfileCard";
+
+function App() {
+    return (
+        <div>
+            <div>Personal digital Assistants</div>
+
+            <ProfileCard />
+            <ProfileCard />
+            <ProfileCard />
+        </div>
+    );
+}
+
+export default App;
