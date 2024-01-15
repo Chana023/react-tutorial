7583c4cbeeea7e54e09c42db834e26d092093719
Chana023 <jesalchana23@gmail.com>
2024-01-15 13:33:10 +0200

    Added images and bulma for styling, learned basics of props

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,13 +1,34 @@
 import ProfileCard from "./ProfileCard";
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
+import 'bulma/css/bulma.css'
 
 function App() {
     return (
         <div>
             <div>Personal digital Assistants</div>
 
-            <ProfileCard title="Alexa" handle="@alexa99" />
-            <ProfileCard title="Cortana" handle="@cortana99" />
-            <ProfileCard title="Siri" handle="@siri99" />
+            <div className="container">
+                <section className="section">
+                    <div className="columns">
+                        <div className=" column is-3">
+                            <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage}/>
+                        </div>
+                        <div className=" column is-3">
+                            <ProfileCard title="Cortana" handle="@cortana99" image={CortanaImage}/>
+                        </div>
+                        <div className=" column is-3">
+                            <ProfileCard title="Siri" handle="@siri99" image={SiriImage}/>
+                        </div>
+
+                    </div>
+
+                </section>
+            </div>
+            
+            
+            
         </div>
     );
 }
