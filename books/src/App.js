86fee4d025acf4375b9dba46916ac6e7a6a1a47b
Chana023 <jesalchana23@gmail.com>
2024-02-12 15:33:54 +0200

    Completed book delete tutorial

--- books/src/App.js
+++ books/src/App.js
@@ -5,6 +5,14 @@ import BookList from './components/BookList';
 function App() {
     const [books, setBooks] = useState([]);
 
+    const deleteBookById = (id) => {
+        const updateBooks = books.filter((book) => {
+            return book.id !== id;
+        });
+
+        setBooks(updateBooks);
+    };
+
     const createBook = (title) => {
         const updateBooks = [
             ...books, 
@@ -15,7 +23,7 @@ function App() {
 
     return (
     <div className="app">
-        <BookList books={books}/>
+        <BookList books={books} onDelete = {deleteBookById}/>
         <BookCreate onCreate={createBook}/>
     </div>);
 }
