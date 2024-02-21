b0c98fcf0c4628e0638d23538365d6950f9eabbb
Chana023 <jesalchana23@gmail.com>
2024-02-21 19:39:50 +0200

    Completed refactor of app to use applicaton context

--- books/src/components/BookShow.js
+++ books/src/components/BookShow.js
@@ -1,12 +1,14 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import BooksContext from "../context/books";
 
 import BookEdit from './BookEdit'
 
-function BookShow({ book, onDelete, onEdit }) {
+function BookShow({ book }) {
     const [showEdit, setShowEdit] = useState(false);
+    const { deleteBookById } = useContext(BooksContext);
 
     const handleDeleteClick = () => {
-        onDelete(book.id);
+        deleteBookById(book.id);
     };
 
     const handleEditClick = () => {
@@ -14,9 +16,8 @@ function BookShow({ book, onDelete, onEdit }) {
 
     };
 
-    const handleSubmit = (id, newTitle) => {
+    const handleSubmit = () => {
         setShowEdit(false);
-        onEdit(id, newTitle)
     };
 
     let content = <h3>{book.title}</h3>;
