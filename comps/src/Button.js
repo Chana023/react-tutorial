de02d7dcaa814679e6dfb63b1ed5f793d87baa4d
Chana023 <jesalchana23@gmail.com>
2024-05-12 18:20:29 +0200

    Added primary button styling

--- comps/src/Button.js
+++ comps/src/Button.js
@@ -12,13 +12,13 @@ function Button({
     
 
  }) {
-    return <button className="bg-red-500">{children}</button>;
+    return <button className="px-3 py-1.5 border border-blue-500 bg-blue-500 text-white">{children}</button>;
 }
 
 Button.propTypes = {
     checkVariationValue: ({primary , secondary, success, warning, danger }) => {
         const count = Number(!!primary) 
-        + Number(!!warning)
+        + Number(!!secondary)
         + Number(!!warning)
         + Number(!!success)
         + Number(!!danger)
