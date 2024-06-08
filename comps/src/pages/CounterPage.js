4fc126edd5e6a650e22e67edcff259eebf8e1555
Chana023 <jesalchana23@gmail.com>
2024-06-08 16:05:09 +0200

    Completed initial set up to learn about reducers

--- comps/src/pages/CounterPage.js
+++ comps/src/pages/CounterPage.js
@@ -1,18 +1,57 @@
 import Button from '../components/Button'
-import useCounter from '../hooks/use-counter';
+import { useReducer } from 'react';
+import Panel from '../components/Panel'
+
+const reducer = (state, action) => {
+
+};
 
 
 function Counterpage({ initialCount }) {
+    // const [count, setCount] = useState(initialCount)
+    // const [ valueToAdd, setValueToAdd ] = useState(0);
+    const [ state, dispatch ] = useReducer(reducer, {
+        count: initialCount,
+        valueToAdd: 0
+    });
 
-    const {count, increment} = useCounter(initialCount);
+    const increment = () => {
+        // setCount(count + 1)
+        
+    };
+
+
+    const decrement = () => {
+        // setCount(count - 1)
+    };
+
+    const handleChange = (event) => {
+        const value = parseInt(event.target.value) || 0;
+
+        // setValueToAdd(value)
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        // setCount(count + valueToAdd);
+        // setValueToAdd(0);
+    };
 
     return (
-        <div>
-            <h1>
-                count is: {count}
-                <Button onClick={increment}>Increment</Button>
-            </h1>
-        </div>
+        <Panel className='m-3'>
+            <h1 className='text-lg'>count is: {count}</h1> 
+                <div className='flex flex-row'>
+                    <Button onClick={increment}>Increment</Button>
+                    <Button onClick={decrement}>Decrement</Button>
+                </div>
+
+                <form onSubmit={handleSubmit}>
+                    <label>Add a lot!</label>
+                    <input value={valueToAdd || ""} onChange={handleChange} type="number" className="p-1 m-3 bg-gray-50 border border-gray-300"/>
+                    <Button>Add it</Button>
+                </form>
+        </Panel>
     )
 
 }
