279dc75681330b5c9b575da36ca097d3ade83b57
Chana023 <jesalchana23@gmail.com>
2024-06-12 19:54:24 +0200

    Completed adding albums with redux query toolkit

--- media/src/components/AlbumsListItem.js
+++ media/src/components/AlbumsListItem.js
@@ -1,12 +1,19 @@
 import Button from "./Button";
 import ExpandablePanel from "./ExpandablePanel";
 import { GoTrashcan } from "react-icons/go";
+import { useRemoveAlbumMutation } from "../store";
 
 function AlbumListItem({ album }) {
-    const header = <div>
-        <Button><GoTrashcan /></Button>
+    const [ removeAlbum, results ] = useRemoveAlbumMutation();
+
+    const handleRemoveButton = () => {
+        removeAlbum(album);
+    };
+
+    const header = <>
+        <Button classname = "mr-2" loading={results.isLoading} onClick={handleRemoveButton}><GoTrashcan /></Button>
         {album.title}
-        </div>
+        </>
       return (
       <ExpandablePanel key={album.id} header={header}>
         List of Photos in Album
