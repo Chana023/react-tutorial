a1908c3c1439f0d475bd0384912c5634bcd847fa
Chana023 <jesalchana23@gmail.com>
2024-06-14 14:15:41 +0200

    Completed introduction to react router as add on to course

--- registry/src/App.tsx
+++ registry/src/App.tsx
@@ -1,6 +1,35 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Root from './pages/Root'
+import HomePage from "./pages/HomePage"
+import SearchPage from "./pages/search/SearchPage"
+import DetailsPage from "./pages/DetailsPage"
+import { searchLoader } from "./pages/search/searchLoader"
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <HomePage/>
+      },
+      {
+        path: "/search",
+        element: <SearchPage/>,
+        loader: searchLoader,
+      },
+      {
+        path: 'packages/:name',
+        element: <DetailsPage/>
+      },
+    ]
+  }
+])
 
 function App() {
-  return <div>Hi there</div>
+
+  return <RouterProvider router={router}/>
 }
 
 export default App
